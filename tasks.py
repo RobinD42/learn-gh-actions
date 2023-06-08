1a596a936a3987459d5ca17d28219b44bb43c728
Robin Dunn <>
2023-06-07 20:47:25 -0700

    fix version task

--- tasks.py
+++ tasks.py
@@ -4,16 +4,18 @@ from invoke import task, run
 
 VERSION="12.34"
 
+def get_version():
+    result = run('git rev-parse --short=8 HEAD', echo=False, hide=True)
+    git_rev = result.stdout.strip()
+    return f'{VERSION}-{git_rev}'
 
 @task
 def version(ctx):
-    result = run('git rev-parse --short=8 HEAD', echo=True)
-    git_rev = result.stdout
-    print(f'{VERSION}-{git_rev}')
+    return get_version()
 
 @task
 def build(ctx):
     os.makedirs('dist', exist_ok=True)
-    with open(f'dist/my_archive-{version()}.zzz', 'w') as f:
+    with open(f'dist/my_archive-{get_version()}.zzz', 'w') as f:
         f.write('This is just a dummy file to simulate having built something.')
 
